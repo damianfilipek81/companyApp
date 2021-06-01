d9fa14a95186adddca2f829b7478da681088308b
Damian <damianfilipek81@gmail.com>
2021-06-01 18:41:59 +0200

    Add endpoints to mongoDB

--- server.js
+++ server.js
@@ -12,28 +12,14 @@ mongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUn
   }
   else {
     console.log('Successfully connected to the database');
+    
     const db = client.db('companyDB');
-
-    db.collection('employees').find({ department: 'IT' }, (err, data) => {
-      if(!err) {
-        data.each((error, employee) => {
-          console.log(employee);
-        })
-      };
-    });
-
-    db.collection('departments').insertOne({ name: 'Management' }, err => {
-      if(err) console.log('err');
-    });
-
-    db.collection('employees').updateOne({ department: 'IT' }, { $set: { salary: 6000 }}, err => {
-      if(err) console.log(err);
-    });
-
-    db.collection('departments').deleteOne({ name: 'Management' }, (err) => {
-      if(err) console.log(err);
-    });
     const app = express();
+
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
 
     app.use(cors());
     app.use(express.json());
